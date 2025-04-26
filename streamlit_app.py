9fb2dc821fbf21d250769335c958fa8fbb026c78
Prazzzs <adiangga998@gmail.com>
2025-04-26 08:19:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 Sipilah")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.title("Mass saya ngantukkk")
